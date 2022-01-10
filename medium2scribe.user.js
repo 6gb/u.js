bfdda87842e291b7649097ac5d5afa1aa2fbd5a5
6gb <63697672+6gb@users.noreply.github.com>
2022-01-10 20:59:43 -0300

    Update medium2scribe.user.js

--- medium2scribe.user.js
+++ medium2scribe.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            medium2scribe
 // @author          6gb
-// @version         2022.01.10.21
+// @version         2022.01.11
 // @downloadURL     https://raw.githubusercontent.com/6gb/u.js/main/medium2scribe.user.js
 // @description     Redirects medium based pages to scribe.rip. To add new sites, include its domain in a new @match below.
 // @match     *://*.medium.com/*
