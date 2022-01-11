5571fd8751ededab2f8c1fefaeb718f4c2849a82
6gb <63697672+6gb@users.noreply.github.com>
2022-01-10 21:22:07 -0300

    Update medium2scribe.user.js

--- medium2scribe.user.js
+++ medium2scribe.user.js
@@ -2,8 +2,8 @@
 // @name            medium2scribe
 // @author          6gb
 // @version         2022.01.11
-// @downloadURL     https://raw.githubusercontent.com/6gb/u.js/main/medium2scribe.user.js
-// @description     Redirects medium based pages to scribe.rip. To add new sites, include its domain in a new @match below.
+// @namespace       https://6gb.github.io/
+// @description     Redirects medium based pages to scribe.rip. To add new sites, include the domain in a new @match below.
 // @match     *://*.medium.com/*
 // @match     *://*.plainenglish.io/*
 // @match     *://*.baos.pub/*
@@ -17,6 +17,9 @@
 // @match     *://*.infosecwriteups.com/*
 // @match     *://*.uxplanet.org/*
 // @run-at          document-start
+// @license         MIT
+// @updateURL       https://openuserjs.org/meta/6gb/medium2scribe.meta.js
+// @downloadURL     https://openuserjs.org/install/6gb/medium2scribe.user.js
 // ==/UserScript==
 
 location.replace(location.protocol + '//' + location.hostname.replace(/[^.]+\.[^.]+$/, 'scribe.rip') + location.pathname);
